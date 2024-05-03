2e33a14ae6c1333851c83047e52e9f1db88bc8d4
AmrMaree <113312326+AmrMaree@users.noreply.github.com>
2024-05-03 03:39:10 +0300

    commit

--- Road Map Construction/Graph.cpp
+++ Road Map Construction/Graph.cpp
@@ -252,7 +252,7 @@ void Graph :: Prim(string startCity) {
 
     unordered_map<string, bool> visited;
     priority_queue<pair<int, string>, vector<pair<int, string>>, greater<pair<int, string>>> pq;
-    vector<pair<string, int>> MST; // Store MST edges
+    vector<pair<string, string>> MST; // Store MST edges
 
     // Mark all cities as unvisited
     for (auto pair : cities) {
